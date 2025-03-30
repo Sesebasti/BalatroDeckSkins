1e3ec5aa767d220eb361d7b3f616581ade3e7168
Sebastián Escudero <116760721+Sesebasti@users.noreply.github.com>
2025-03-30 11:24:08 -0300

    Add Lethal Company

---
+++ Lethal Company/Lethal Company.lua
@@ -0,0 +1,52 @@
+--- STEAMODDED HEADER
+--- MOD_NAME: Lethal Company
+--- MOD_ID: lethal_company
+--- PREFIX: lc
+--- MOD_AUTHOR: [Sonicite, SeSebasti]
+--- MOD_DESCRIPTION: Replace the Diamonds face cards to the Employee, the Ghost Girl and the Nutcracker from Lethal Company.
+
+local atlas_key = 'lc_atlas' -- Format: PREFIX_KEY
+-- See end of file for notes
+local atlas_path = 'lethal.png' -- Filename for the image in the asset folder
+local atlas_path_hc = 'lethal_hc.png' -- Filename for the high-contrast version of the texture, if existing
+
+local suits = {'diamonds'} -- Which suits to replace
+local ranks = {'Jack', 'Queen', "King"} -- Which ranks to replace
+
+local description = 'Lethal Company' -- English-language description, also used as default
+
+-----------------------------------------------------------
+-- You should only need to change things above this line --
+-----------------------------------------------------------
+
+SMODS.Atlas{  
+    key = atlas_key..'_lc',
+    px = 71,
+    py = 95,
+    path = atlas_path,
+    prefix_config = {key = false}, -- See end of file for notes
+}
+
+if atlas_path_hc then
+    SMODS.Atlas{  
+        key = atlas_key..'_hc',
+        px = 71,
+        py = 95,
+        path = atlas_path_hc,
+        prefix_config = {key = false}, -- See end of file for notes
+    }
+end
+
+for _, suit in ipairs(suits) do
+    SMODS.DeckSkin{
+        key = suit.."_skin",
+        suit = suit:gsub("^%l", string.upper),
+        ranks = ranks,
+        lc_atlas = atlas_key..'_lc',
+        hc_atlas = (atlas_path_hc and atlas_key..'_hc') or atlas_key..'_lc',
+        loc_txt = {
+            ['en-us'] = description
+        },
+        posStyle = 'collab'
+    }
+end
