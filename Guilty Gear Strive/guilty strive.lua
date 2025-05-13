6ead5c1a6f6c64b52b55352be19f2ab2c42ca14a
Sebastián Escudero <116760721+Sesebasti@users.noreply.github.com>
2025-05-13 15:57:59 -0400

    Add Guilty Gear Strive

---
+++ Guilty Gear Strive/guilty strive.lua
@@ -0,0 +1,51 @@
+--- STEAMODDED HEADER
+--- MOD_NAME: Guilty Gear: Strive
+--- MOD_ID: guiltygearstrive
+--- PREFIX: ggstFT
+--- MOD_AUTHOR: [flwr_venus, SeSebasti]
+--- MOD_DESCRIPTION: Adds face cards based on Sol, Jack-O and Asuka from GGST, and slightly colours the whole deck.
+
+local atlas_key = 'ggstFT_atlas' -- Format: PREFIX_KEY
+-- See end of file for notes
+local atlas_path = 'ggstFT.png' -- Filename for the image in the asset folder
+
+local suits = {'hearts', 'clubs', 'diamonds', 'spades'} -- Which suits to replace
+local ranks = {'2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', "King", "Ace",} -- Which ranks to replace
+
+local description = 'Guilty Gear: Strive' -- English-language description, also used as default
+
+-----------------------------------------------------------
+-- You should only need to change things above this line --
+-----------------------------------------------------------
+
+SMODS.Atlas{  
+    key = atlas_key..'_lc',
+    px = 71,
+    py = 95,
+    path = atlas_path,
+    prefix_config = {key = false}, -- See end of file for notes
+}
+
+if atlas_path_hc then
+    SMODS.Atlas{  
+        key = atlas_key..'_hc',
+        px = 71,
+        py = 95,
+        path = atlas_path_hc,
+        prefix_config = {key = false}, -- See end of file for notes
+    }
+end
+
+for _, suit in ipairs(suits) do
+    SMODS.DeckSkin{
+        key = suit.."_skin",
+        suit = suit:gsub("^%l", string.upper),
+        ranks = ranks,
+        lc_atlas = atlas_key..'_lc',
+        hc_atlas = (atlas_path_hc and atlas_key..'_hc') or atlas_key..'_lc',
+        loc_txt = {
+            ['en-us'] = description
+        },
+        posStyle = 'deck'
+    }
+end
